14da9c5d8722ddc640f2f40ada0891e7ffb7d18d
AttilaJager <jagerattila@gmail.com>
2024-01-12 23:45:53 +0100

    Init

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { BaseService } from '../base.service';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  messages:any
+  newMessage:any
+  userName="Attila"
+  constructor(private base:BaseService) {
+    this.base.getMessages().snapshotChanges().pipe(
+      map(
+        (ch)=> ch.map(
+          (c)=>({key: c.payload.key, ...c.payload.val()})
+        )
+      )
+    ).subscribe(
+      (res)=>this.messages=res
+    )
+  }
+
+
+  addMessage(){
+    if (this.newMessage){
+      let time= new Date().toLocaleTimeString()
+      let body = {user:this.userName, time:time, message:this.newMessage}
+      this.base.addMessage(body)
+      this.newMessage=""
+     }
+  }
+
+  updateMessage(message:any){
+    this.base.updateMessage(message)
+  }
+
+
+  deleteMessage(message:any){
+    this.base.deleteMessage(message)
+  }
+}
